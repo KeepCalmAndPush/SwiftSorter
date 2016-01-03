2c88b70f7b427de0bb00b9a6dde0f096ef0fde0c
Andrey <andrey-the-first@yandex.ru>
2016-01-03 18:02:38 +0300

    Initial Commit

---
+++ SwiftSorter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSorter
+//
+//  Created by Андрей on 03.01.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
